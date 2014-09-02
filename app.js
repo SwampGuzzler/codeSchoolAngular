c31337150543c374ca1fae263e5a65800552db97
Lucas Cotner <lc07@uw.edu>
2014-09-02 11:05:59 -0700

    Working directives, modules, expressions

---
+++ app.js
@@ -0,0 +1,7 @@
+
+var app = angular.module("store", []);
+/* Where app is the name of the application, angular references the
+Angular.js library, .module creates a new module, 'store' is the name of the application, and
+the empty array are the app's dependencies, of which there are 
+currently none. */
+
