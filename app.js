83161effbd48457d5cc50fddb8a50c290f678d88
Lucas Cotner <lc07@uw.edu>
2014-09-02 15:54:11 -0700

    Working filters

--- app.js
+++ app.js
@@ -8,14 +8,25 @@
 	// The controller name should always be capitalized and have controller in it
 	// 2nd parameter is anonymous function that will be exectuted when our 
 	// StoreController is called. 
-		this.product = gem; // product is a property of our Controller
+		this.products = gems; // product is a property of our Controller
 	});
 
-	var gem = {
-		name: 'Topazz',
-		price: 20.95,
-		description: "pretty"
-	};
+	var gems = [
+		{
+			name: 'Topazz',
+			price: 20.95,
+			description: "pretty",
+			canPurchase: true,
+			images: []
+		},
+		{
+			name: 'Emerald',
+			price: 29.0,
+			description: "beautiful",
+			canPurchase: true,
+			images: []
+		}
+	];
 
 })();
 
