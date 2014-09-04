687e10052c1abd03e9b12203748714dc735f1e4c
Lucas Cotner <lc07@uw.edu>
2014-09-04 11:23:57 -0700

    Dependency Injection

--- products.js
+++ products.js
@@ -3,6 +3,20 @@
 	// & Even though we are calling both modules 'app' (like we did in 
 	// the app.js file), this still works out because they are both 
 	// wrapped in a closure (see line 1!).
+	app.controller('StoreController', [ '$http', function($http) { 
+		// DEPENDENCY INJECTION!!
+		//this.products = ??
+		var store = this; //avoids the scoping problem detailed below!
+		store.products = []; // we initialize products here b/c now the page can
+		// load w/o waiting for the data that'll be returned from our GET request
+		$http.get('/products.json').success(function(data) {
+			// That function after success is a callback b/c we know this success 
+			// function will return some data.
+			// Also. we can't just set 'this.products = data', because inside of this
+			// callback, the 'this' keyword is now pointing at $http!!
+			store.products = data;
+		});
+	}]);
 
 	app.directive('productTitle', function() {
 		return {
